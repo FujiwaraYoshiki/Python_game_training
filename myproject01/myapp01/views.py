1847b5cfa28b3ac9cf9a757393f4cdc06e0f2d14
FujiwaraYoshiki <sakusakusakuragi@gmail.com>
2024-11-17 23:03:31 +0900

    [Django]Create:myproject01  - Creating forms utilizing form classes

--- myproject01/myapp01/views.py
+++ myproject01/myapp01/views.py
@@ -1,17 +1,23 @@
 # myapp01/views.py
 from django.shortcuts import render
+from .forms import NameForm
 
 
 def index(request):
     if request.method == "POST":
-        # フォームから送られたデータを取得
-        name = request.POST.get('name', '')
-        message = f"Hello, {name}! Nice to meet you!"
+        form = NameForm(request.POST)
+        if form.is_valid():
+            # フォームが有効ならデータを取り出す
+            name = form.cleaned_data['name']
+            message = f"Hello, {name}! Nice to meet you!"
+        else:
+            message = "Invalid input. Please try again."
     else:
-        # デフォルトのメッセージ
+        form = NameForm()
         message = "Hello! Please enter your name."
 
     context = {
-        'message': message
+        'form': form,
+        'message': message,
     }
     return render(request, 'myapp01/index.html', context)
